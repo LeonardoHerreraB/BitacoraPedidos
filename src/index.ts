53dc6be333ef6167ee69cb525a9323c477b9c13f
leonardoherrerab <leonardo.herrera.barrero@outlook.com>
2023-08-20 12:36:40 -0700

    commit_2

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, {Request, Response} from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+//iniciamos una instancia del express
+const app = express();
+const corsoptions = {
+    origin : '*'
+}
+app.use(cors(corsoptions));
+app.use(express.json());
+app.use('/', (req: Request, res: Response)=>{res.send("HolaMundo");})
+export default app;
---
+++ src/servers.ts
@@ -0,0 +1,4 @@
+import app from './index';
+
+const port = 4000;
+app.listen( port, ()=>{console.log("se ha iniciado correctamente en el puerto")})
